1ca60dd039e65c7aba23c204f6c1d81837ae840e
Regi Shehi <regi_shehi@hotmail.com>
2024-11-14 18:53:13 +0100

    Add initial migration

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,15 @@
+﻿namespace AuctionService.Entities;
+
+public record Auction
+{
+    public Guid Id { get; init; }
+    public int ReservePrice { get; init; }
+    public string? Seller { get; init; }
+    public string? Winner { get; init; }
+    public int? CurrentHighBid { get; init; }
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; init; } = DateTime.UtcNow;
+    public DateTime AuctionEnd { get; init; }
+    public Status Status { get; init; }
+    public Item? Item { get; init; }
+}
