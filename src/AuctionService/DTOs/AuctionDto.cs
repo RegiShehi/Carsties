0f21a2392483c2bb4bfa96b016df5afb9c70ac04
Regi Shehi <regi_shehi@hotmail.com>
2024-11-14 23:36:54 +0100

    Add mapping profiles

---
+++ src/AuctionService/DTOs/AuctionDto.cs
@@ -0,0 +1,20 @@
+﻿namespace AuctionService.DTOs;
+
+public class AuctionDto
+{
+    public Guid Id { get; init; }
+    public int ReservePrice { get; init; }
+    public string? Seller { get; init; }
+    public string? Winner { get; init; }
+    public int CurrentHighBid { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+    public DateTime AuctionEnd { get; init; }
+    public string? Status { get; init; }
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public string? Color { get; set; }
+    public int Year { get; set; }
+    public int Mileage { get; set; }
+    public string? ImageUrl { get; set; }
+}
---
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionService.DTOs;
+
+public class UpdateAuctionDto
+{
+    [Required]
+    public required string Make { get; set; }
+
+    [Required]
+    public required string Model { get; set; }
+
+    [Required]
+    public required string Color { get; set; }
+
+    [Required]
+    public int Year { get; set; }
+
+    [Required]
+    public int Mileage { get; set; }
+}
