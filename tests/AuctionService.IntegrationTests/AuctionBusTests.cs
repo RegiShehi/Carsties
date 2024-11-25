a4da27cdd710cc8ed1c949b57bfde660888fdda4
Regi Shehi <regi_shehi@hotmail.com>
2024-11-25 21:26:25 +0100

    Test service bus

---
+++ tests/AuctionService.IntegrationTests/AuctionBusTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.IntegrationTests.Fixtures;
+using AuctionService.IntegrationTests.Util;
+using Contracts;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService.IntegrationTests;
+
+public class AuctionBusTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+{
+    private readonly CustomWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+    private readonly ITestHarness _testHarness;
+
+    public AuctionBusTests(CustomWebAppFactory factory)
+    {
+        _factory = factory;
+        _httpClient = _factory.CreateClient();
+        _testHarness = factory.Services.GetTestHarness();
+    }
+
+    [Fact]
+    public async Task CreateAuction_WithValidObject_PublishesAuctionCreatedEvent()
+    {
+        // arrange
+        var auction = GetAuctionForCreate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("Bob"));
+
+        // act
+        var response = await _httpClient.PostAsJsonAsync("api/auctions", auction);
+
+        // assert
+        response.EnsureSuccessStatusCode();
+        Assert.True(await _testHarness.Published.Any<AuctionCreated>());
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+        DbHelper.ReInitDbForTests(db);
+
+        return Task.CompletedTask;
+    }
+
+    private CreateAuctionDto GetAuctionForCreate()
+    {
+        return new CreateAuctionDto
+        {
+            Make = "test make",
+            Model = "test model",
+            ImageUrl = "test url",
+            Color = "test",
+            Mileage = 10,
+            Year = 2020,
+            ReservePrice = 10
+        };
+    }
+}
